19779a77fc9759273c071c29b5cb90f128696652
FaithAyo <faithojetayo@gmail.com>
2023-06-19 16:33:56 +0000

    update

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python3
 def common_elements(set_1, set_2):
-    uniq = [x for x in set_1 for y in set_2 if x == y]
-    return uniq
+    set_3 = set_1 & set_2
+    return set_3
