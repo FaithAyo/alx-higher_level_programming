2456bd12046e65cd99940fcef0e3cf67d12e30e4
FaithAyo <faithojetayo@gmail.com>
2023-06-19 10:18:33 -0700

    README.md

--- 0x03-python-data_structures/1-element_at.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def element_at(my_list, idx):
-    if idx > len(my_list) - 1:
-        return None
-    elif idx < 0:
-        return None
-    for element in my_list:
-        if my_list.index(element) == idx:
-            return element
